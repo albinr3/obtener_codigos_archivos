83db619522e138d160995fb0fb9ded32ba2f1070
Albin Rodriguez <albinmrodriguez@gmail.com>
2022-03-18 08:34:28 -0400

    primer commit

---
+++ obtener codigos de columnas.py
@@ -0,0 +1,29 @@
+import xlrd, os
+
+contador = 0
+dirs = os.listdir()
+lista = []
+lista_final_sinvacios = []
+lista_final_sinletras = []
+def es_numero(n):
+    try:
+        float(n)
+    except ValueError:
+        return False
+    return True
+
+for file in dirs:
+    if file.endswith(".xls"):
+        print(file)
+        data = xlrd.open_workbook(file)
+        sheet1 = data.sheet_by_index(0)
+        a = sheet1.col_values(3)
+        lista.extend(a)
+
+lista_final_sinvacios = [string for string in lista if string !=""]  
+
+for elem in lista_final_sinvacios:
+    if es_numero(elem):
+        lista_final_sinletras.append(float(elem))
+         
+print(len(lista_final_sinletras))
